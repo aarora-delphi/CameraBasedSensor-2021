5dec5c8482525299cf027fed14db5f0b1b7b937e
ISA Box <gbanuru@uci.edu>
2021-11-26 21:40:52 -0800

    Added nginx web server files and updated path to root folder

--- script/boot.sh
+++ script/boot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LOG='/home/delphidisplay/Desktop/CameraBasedSensor-2021/log/boot.log'
+LOG='/home/delphi/Desktop/CameraBasedSensor-2021/log/boot.log'
 
 FUNC_NOTIFY () {
 CURRTIME="$(date '+%m-%d-%Y %H:%M:%S')"
@@ -17,30 +17,35 @@ fi
 }
 
 FUNC_RUNOAK () {
-gnome-terminal --geometry='70x20' --title=RUNOAK --working-directory=/home/delphidisplay/Desktop/CameraBasedSensor-2021 -- venv/bin/python3 runsim.py -track -full &
+gnome-terminal --geometry='70x20' --title=RUNOAK --working-directory=/home/delphi/Desktop/CameraBasedSensor-2021 -- venv/bin/python3 runsim.py -track -full &
 sleep 2
 xdotool search -name RUNOAK windowminimize
 }
 
-FUNC_FLASKAPP () {
-gnome-terminal --geometry='70x20' --title=FLASKAPP --working-directory=/home/delphidisplay/Desktop/CameraBasedSensor-2021 -- venv/bin/python3 app/wsgi.py &
-sleep 2
-xdotool search -name FLASKAPP windowminimize
-}
+#FUNC_FLASKAPP () {
+#gnome-terminal --geometry='70x20' --title=FLASKAPP --working-directory=/home/delphi/Desktop/CameraBasedSensor-2021 -- venv/bin/python3 app/wsgi.py &
+#sleep 2
+#xdotool search -name FLASKAPP windowminimize
+#}
 
 while :
 do
 
-sleep 5
+sleep 10
 
 if [[ ! $(wmctrl -l) =~ "RUNOAK" ]]; then
-    FUNC_NOTIFY "RUNOAK Window is Missing - Executing FUNC_RUNOAK"
-    FUNC_RUNOAK
+    FUNC_NOTIFY "RUNOAK Window is Missing - Confirming in 5 seconds"
+    sleep 5
+    if [[ ! $(wmctrl -l) =~ "RUNOAK" ]]; then
+        FUNC_NOTIFY "RUNOAK Window is Missing Confirmed - Executing FUNC_RUNOAK"
+        FUNC_RUNOAK
+    fi
 fi
 
-if [[ ! $(wmctrl -l) =~ "FLASKAPP" ]]; then
-    FUNC_NOTIFY "FLASKAPP Window is Missing - Executing FUNC_FLASKAPP"
-    FUNC_FLASKAPP
-fi
+
+#if [[ ! $(wmctrl -l) =~ "FLASKAPP" ]]; then
+#    FUNC_NOTIFY "FLASKAPP Window is Missing - Executing FUNC_FLASKAPP"
+#    FUNC_FLASKAPP
+#fi
 
 done
