e54890d761bc362dddcb44299328b16bf5676976
Gautam <gbanuru@delphidisplay.com>
2021-12-23 19:50:00 -0800

    deleted repo

--- app/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""Application entry point."""
-
-### local-packages
-from project import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=2000)
--- script/boot_hook.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-BOOT_HANDLER="gnome-terminal --geometry='70x20' --title=BOOT -- bash -c '/home/delphi/Desktop/CameraBasedSensor-2021/script/boot.sh'"
-
-sleep 10
-$($BOOT_HANDLER)
-sleep 2
-xdotool search -name BOOT windowminimize
